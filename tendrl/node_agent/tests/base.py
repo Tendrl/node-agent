12853998852c6802d2ae3c0c33a4e245065df415
Rohan Kanade <rkanade@redhat.com>
2016-10-12 17:54:29 +0530

    Removes oslo deps, get everything under "tendrl.*" namespace

---
+++ tendrl/node_agent/tests/base.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
+class TestCase(object):
+
+    """Test case base class for all unit tests."""
---
+++ tendrl/node_agent/tests/test_node_agent.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+test_node_agent
+----------------------------------
+
+Tests for `node_agent` module.
+"""
+
+from tendrl.node_agent.tests import base
+
+
+class TestNodeAgent(base.TestCase):
+
+    def test_something(self):
+        assert True
