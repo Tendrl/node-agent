8e22401f59d34daa5e861f02e7def6cd30d6a584
Shubhendu <shtripat@redhat.com>
2016-11-11 11:01:12 +0530

    Incorporated review comments
    Signed-off-by: Shubhendu <shtripat@redhat.com>

--- tendrl/node_agent/flows/flow.py
+++ tendrl/node_agent/flows/flow.py
@@ -1,36 +1,68 @@
+import logging
+
+LOG = logging.getLogger(__name__)
+
+
 class Flow(object):
-    def __init__(self, api_job):
-        self.api_job = api_job
+    def __init__(self, job):
+        self.job = job
 
     def run(self):
-        parameters = self.api_job['parameters']
+        parameters = self.job['parameters']
+        parameters.update({'status': []})
 
         # Execute the pre runs for the flow
+        LOG.info("Starting execution of pre-runs for flow: %s" %
+                 self.job['flow'])
         for func in self.pre_run:
             mod_name = func[:func.rfind('.')]
             class_name = func[func.rfind('.') + 1:]
             ret_val = getattr(mod_name, class_name)(). \
-                run(self.api_job['parameters'])
+                run(parameters)
             if not ret_val:
+                LOG.error("Failed executing pre-run: %s for flow: %s" %
+                          (func, self.job['flow']))
                 raise Exception(
-                    "Error executing pre run function: %s" % func
+                    "Error executing pre run function: %s for flow: %s" %
+                    (func, self.job['flow'])
                 )
+            else:
+                LOG.info("Successfully executed pre-run: %s for flow: %s" %
+                         (func, self.job['flow']))
 
         # Execute the atoms for the flow
+        LOG.info("Starting execution of atoms for flow: %s" %
+                 self.job['flow'])
         for atom in self.atoms:
             mod_name = func[:func.rfind('.')]
             class_name = func[func.rfind('.') + 1:]
             ret_val = getattr(mod_name, class_name)(). \
                 run(parameters)
-            parameters.update(ret_val)
+            if not ret_val:
+                LOG.error("Failed executing atom: %s on flow: %s" %
+                          (func, self.job['flow']))
+                raise Exception(
+                    "Error executing atom: %s on flow: %s" %
+                    (atom, self.job['flow'])
+                )
+            else:
+                LOG.info('Successfully executed atoms for flow: %s' %
+                         self.job['flow'])
 
         # Execute the post runs for the flow
+        LOG.info("Starting execution of post-runs for flow: %s" %
+                 self.job['flow'])
         for func in self.post_run:
             mod_name = func[:func.rfind('.')]
             class_name = func[func.rfind('.') + 1:]
             ret_val = getattr(mod_name, class_name)(). \
-                run(self.api_job['parameters'])
+                run(parameters)
             if not ret_val:
+                LOG.error("Failed executing post-run: %s for flow: %s" %
+                          (func, self.job['flow']))
                 raise Exception(
                     "Error executing post run function: %s" % func
                 )
+            else:
+                LOG.info("Successfully executed post-run: %s for flow: %s" %
+                         (func, self.job['flow']))
