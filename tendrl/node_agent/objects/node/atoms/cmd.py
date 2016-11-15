b69cbd31b2a75e4bdd1592977a13bf5c80795a17
Rohan Kanade <rkanade@redhat.com>
2016-11-15 20:06:36 +0530

    Fix objects.node.atoms.cmd

--- tendrl/node_agent/objects/node/atoms/cmd.py
+++ tendrl/node_agent/objects/node/atoms/cmd.py
@@ -1,26 +1,11 @@
 import logging
-
-from tendrl.node_agent.ansible_runner.ansible_module_runner \
-    import AnsibleExecutableGenerationFailed
-from tendrl.node_agent.ansible_runner.ansible_module_runner \
-    import AnsibleRunner
-
+import subprocess
 
 LOG = logging.getLogger(__name__)
-ANSIBLE_MODULE_PATH = "core/commands/command.py"
-
 
 class Cmd(object):
     def run(self, parameters):
         cmd = parameters.get("Node.cmd_str")
-        try:
-            runner = AnsibleRunner(
-                ANSIBLE_MODULE_PATH,
-                _raw_params=cmd
-            )
-            result, err = runner.run()
-        except AnsibleExecutableGenerationFailed as ex:
-            LOG.error(ex)
-            return False
-        LOG.info(result)
+        cmd = ["nohup"] + cmd.split(" ")
+        subprocess.Popen(cmd)
         return True
