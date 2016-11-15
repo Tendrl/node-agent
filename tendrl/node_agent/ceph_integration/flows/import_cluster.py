78de9c89b8d3984f1de82e91f31a56ecb9d87ddf
Rohan Kanade <rkanade@redhat.com>
2016-11-15 19:22:52 +0530

    Fix gluster/ceph import flows

--- tendrl/node_agent/ceph_integration/flows/import_cluster.py
+++ tendrl/node_agent/ceph_integration/flows/import_cluster.py
@@ -7,6 +7,7 @@ from tendrl.node_agent.flows.flow import Flow
 
 class ImportCluster(Flow):
     def run(self):
+        cluster_id = self.parameters['Tendrl_context.cluster_id']
         node_list = self.parameters['Node[]']
         if len(node_list) > 1:
             # This is the master node for this flow
@@ -15,7 +16,7 @@ class ImportCluster(Flow):
                     new_params = self.parameters.copy()
                     new_params['Node[]'] = [node]
                 # create same flow for each node in node list except $this
-                    job = {"cluster_id": self.parameters['cluster_id'],
+                    job = {"cluster_id": cluster_id,
                            "node_id": node,
                            "run": self.name,
                            "status": "new",
@@ -28,6 +29,5 @@ class ImportCluster(Flow):
         ceph = "git+https://github.com/Tendrl/ceph_integration"
         self.parameters['Package.name'] = ceph
         self.parameters['Node.cmd_str'] = "tendrl-ceph-integration " \
-                                          "--cluster-id %s" % self.parameters[
-            'Tendrl_context.cluster_id']
+                                          "--cluster-id %s" % cluster_id
         return super(ImportCluster, self).run()
