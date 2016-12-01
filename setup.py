94112dd46316cc91514c4c627f01b054f09216cb
nnDarshan <darshan.n.2024@gmail.com>
2016-12-01 18:38:06 +0530

    Node-agent: Fix setup.py to install requirements.
    Installation of requirements was missing in setup.py.
    Have added the "install_requires" section to install
    all the package dependencies.
    
    tendrl-bug-id: Tendrl/node_agent#80
    
    Signed-off-by: nnDarshan <darshan.n.2024@gmail.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 from setuptools import find_packages
 from setuptools import setup
 
+
+def extract_requirements(filename):
+    with open(filename, 'r') as requirements_file:
+        return [
+            x[:-1] for x in requirements_file.readlines()
+            if not x.startswith("#") and x[:-1] != ''
+        ]
+
+install_requires = extract_requirements('requirements.txt')
+
 setup(
     name="tendrl-node-agent",
     version="0.0.1",
@@ -12,6 +22,7 @@ setup(
     author_email="rkanade@redhat.com",
     license="LGPL-2.1+",
     zip_safe=False,
+    install_requires=install_requires,
     entry_points={
         'console_scripts': [
             'tendrl-node-agent = tendrl.node_agent.manager.manager:main'
