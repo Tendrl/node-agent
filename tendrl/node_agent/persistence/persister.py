884251c770a69355c611058189a7062eb194bd49
Shubhendu <shtripat@redhat.com>
2016-12-21 17:21:46 +0530

    Incorporated more review comments
    tendrl-spec: refactor_common_code_and_move
    tendrl-bug-id: Tendrl/node_agent#102
    
    Signed-off-by: Shubhendu <shtripat@redhat.com>

--- tendrl/node_agent/persistence/persister.py
+++ tendrl/node_agent/persistence/persister.py
@@ -1,5 +1,4 @@
 from tendrl.common.persistence.etcd_persister import EtcdPersister
-from tendrl.common.persistence.file_persister import FilePersister
 
 
 class NodeAgentEtcdPersister(EtcdPersister):
@@ -27,66 +26,3 @@ class NodeAgentEtcdPersister(EtcdPersister):
 
     def update_tendrl_definitions(self, definition):
         self._store.save(definition)
-
-
-class NodeAgentFilePersister(FilePersister):
-    def __init__(self, config):
-        super(NodeAgentFilePersister, self).__init__(config)
-        self._doc_location = "%s/node_agent" % \
-            config.get("node_agent", "doc_persist_location")
-
-    def update_cpu(self, cpu):
-        f = open(
-            "%s/%s" % (self._doc_location, cpu.__name__),
-            "w"
-        )
-        f.write(cpu.json())
-        f.close()
-
-    def update_memory(self, memory):
-        f = open(
-            "%s/%s" % (self._doc_location, memory.__name__),
-            "w"
-        )
-        f.write(memory.json())
-        f.close()
-
-    def update_os(self, os):
-        f = open(
-            "%s/%s" % (self._doc_location, os.__name__),
-            "w"
-        )
-        f.write(os.json())
-        f.close()
-
-    def update_node(self, fqdn):
-        f = open(
-            "%s/%s" % (self._doc_location, fqdn.__name__),
-            "w"
-        )
-        f.write(fqdn.json())
-        f.close()
-
-    def update_node_context(self, context):
-        f = open(
-            "%s/%s" % (self._doc_location, context.__name__),
-            "w"
-        )
-        f.write(context.json())
-        f.close()
-
-    def update_tendrl_context(self, context):
-        f = open(
-            "%s/%s" % (self._doc_location, context.__name__),
-            "w"
-        )
-        f.write(context.json())
-        f.close()
-
-    def update_tendrl_definitions(self, definition):
-        f = open(
-            "%s/%s" % (self._doc_location, definition.__name__),
-            "w"
-        )
-        f.write(definition.json())
-        f.close()
