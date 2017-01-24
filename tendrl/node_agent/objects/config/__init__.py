2ba63b7f92bb577964121686b6b204fa3a56ece4
Rohan Kanade <rkanade@redhat.com>
2017-01-24 13:19:56 +0530

    Node agent defs issue

--- tendrl/node_agent/objects/config/__init__.py
+++ tendrl/node_agent/objects/config/__init__.py
@@ -8,7 +8,7 @@ class Config(objects.NodeAgentBaseObject):
     def __init__(self, config=None, *args, **kwargs):
         super(Config, self).__init__(*args, **kwargs)
 
-        self.value = '_tendrl/config/node-agent/data'
+        self.value = '_tendrl/config/node-agent'
         self.data = config or cmn_config.load_config(
             'node-agent',"/etc/tendrl/node-agent/node-agent.conf.yaml")
         self._etcd_cls = _ConfigEtcd
@@ -18,5 +18,5 @@ class _ConfigEtcd(EtcdObj):
     """Config etcd object, lazily updated
 
     """
-    __name__ = '_tendrl/config/node-agent/'
+    __name__ = '_tendrl/config/node-agent'
     _tendrl_cls = Config
