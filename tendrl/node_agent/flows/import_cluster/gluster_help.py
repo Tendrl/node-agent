5745aa4eb4312416ba1d534397c5d64f4ba3a9ae
Rohan Kanade <rkanade@redhat.com>
2017-01-27 04:09:26 +0530

    Fix greenlet and essential objects startup order

--- tendrl/node_agent/flows/import_cluster/gluster_help.py
+++ tendrl/node_agent/flows/import_cluster/gluster_help.py
@@ -24,7 +24,8 @@ def import_gluster(integration_id):
                        ".yaml", 'w+') as f:
         f.write(logging_file)
 
-    config_data = {"etcd_port": 2379, "etcd_connection": "tendrl-etcd",
+    config_data = {"etcd_port": tendrl_ns.config.data['etcd_port'],
+                   "etcd_connection": tendrl_ns.config.data['etcd_connection'],
                    "tendrl_ansible_exec_file": "$HOME/.tendrl/gluster-integration/ansible_exec",
                    "log_cfg_path":"/etc/tendrl/gluster-integration/gluster-integration_logging"
                        ".yaml", "log_level": "DEBUG"}
@@ -36,7 +37,7 @@ def import_gluster(integration_id):
     with open(gluster_integration_context, 'wb+') as f:
         f.write(integration_id)
 
-    subprocess.Popen(["nohup", "tendrl-gluster-integration"])
+    subprocess.Popen(["nohup", "tendrl-gluster-integration", "&"])
 
 
 
