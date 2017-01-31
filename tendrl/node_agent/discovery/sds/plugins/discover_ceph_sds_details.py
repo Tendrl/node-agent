07da6e66c3952cc833c63c509877f28c438b3571
Shubhendu <shtripat@redhat.com>
2017-01-31 10:53:49 +0530

    Corrected sds detection logic for OSD nodes
    tendrl-bug-id: Tendrl/node-agent#193
    
    Signed-off-by: Shubhendu <shtripat@redhat.com>

--- tendrl/node_agent/discovery/sds/plugins/discover_ceph_sds_details.py
+++ tendrl/node_agent/discovery/sds/plugins/discover_ceph_sds_details.py
@@ -4,6 +4,7 @@ import subprocess
 
 from tendrl.node_agent.discovery.sds.discover_sds_plugin \
     import DiscoverSDSPlugin
+from tendrl.node_agent import ini2json
 
 LOG = logging.getLogger(__name__)
 
@@ -14,7 +15,7 @@ class DiscoverCephStorageSystem(DiscoverSDSPlugin):
 
         # get the gluster version details
         cmd = subprocess.Popen(
-            "ceph version -f json",
+            "ceph --version",
             shell=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
@@ -25,26 +26,16 @@ class DiscoverCephStorageSystem(DiscoverSDSPlugin):
             return ret_val
 
         if out:
-            details = json.loads(out)
+            details = out.split()
 
-            ret_val['pkg_version'] = details['version'].split()[2]
-            ret_val['pkg_name'] = details['version'].split()[0]
+            ret_val['pkg_version'] = details[2]
+            ret_val['pkg_name'] = details[0]
 
             # get the cluster_id details
-            cmd = subprocess.Popen(
-                "ceph -s -f json",
-                shell=True,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE
-            )
-            out, err = cmd.communicate()
-            if err:
-                LOG.error("Error getting cluster details")
-                return ret_val
-            if out:
-                details = json.loads(out)
-                ret_val['detected_cluster_id'] = details['fsid']
-                ret_val['cluster_attrs'] = {'fsid': details['fsid'],
+            raw_data = ini2json.ini_to_dict("/etc/ceph/ceph.conf")
+            if "global" in raw_data:
+                ret_val['detected_cluster_id'] = raw_data['global']['fsid']
+                ret_val['cluster_attrs'] = {'fsid': raw_data['global']['fsid'],
                                             'name': 'ceph'}
 
             return ret_val
