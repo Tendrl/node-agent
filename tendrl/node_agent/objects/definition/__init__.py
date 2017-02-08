5ed61e03ae1b29455240cf29fc7a9aafad2188ce
Rohan Kanade <rkanade@redhat.com>
2017-02-08 20:19:56 +0530

    Fix pep8/unit tests
    tendrl-bug-id: Tendrl/node-agent#206

--- tendrl/node_agent/objects/definition/__init__.py
+++ tendrl/node_agent/objects/definition/__init__.py
@@ -1,5 +1,3 @@
-import importlib
-
 import maps
 import yaml
 
@@ -26,48 +24,27 @@ class Definition(objects.BaseObject):
     def get_obj_definition(self, namespace, obj_name):
         raw_ns = "namespace.%s" % namespace
         raw_obj = self._get_parsed_defs()[raw_ns]['objects'][obj_name]
-        for atom_name, atom in raw_obj.get('atoms', {}).iteritems():
-            atom_mod = atom['run'].split(".atoms.")[-1].split(".")[0]
-            atom_fqdn = "%s.objects.%s.atoms.%s" % (namespace,
-                                                    obj_name.lower(),
-                                                    atom_mod)
-            atom_cls = getattr(importlib.import_module(atom_fqdn), atom_name)
-            tendrl_ns.add_atom(obj_name, atom_name, atom_cls)
-
-        for flow_name, flow in raw_obj.get('flows', {}).iteritems():
-            flow_mod = flow['run'].split(".flows.")[-1].split(".")[0]
-            flow_fqdn = "%s.objects.%s.flows.%s" % (namespace,
-                                                    obj_name.lower(),
-                                                    flow_mod)
-            flow_cls = getattr(importlib.import_module(flow_fqdn), flow_name)
-            tendrl_ns.add_obj_flow(obj_name, flow_name, flow_cls)
-
         return maps.NamedDict(attrs=raw_obj['attrs'],
-                            enabled=raw_obj['enabled'],
-                            obj_list=raw_obj.get('list', ""),
-                            obj_value=raw_obj['value'],
-                            atoms=raw_obj.get('atoms', {}),
-                            flows=raw_obj.get('flows', {}),
-                            help=raw_obj['help'])
+                              enabled=raw_obj['enabled'],
+                              obj_list=raw_obj.get('list', ""),
+                              obj_value=raw_obj['value'],
+                              atoms=raw_obj.get('atoms', {}),
+                              flows=raw_obj.get('flows', {}),
+                              help=raw_obj['help']
+                              )
 
     def get_flow_definition(self, namespace, flow_name):
         raw_ns = "namespace.%s" % namespace
 
         raw_flow = self._get_parsed_defs()[raw_ns]['flows'][flow_name]
-        flow_mod = raw_flow['run'].split(".flows.")[-1].split(".")[0]
-        flow_fqdn = "%s.flows.%s" % (namespace, flow_mod)
-        flow_cls = getattr(importlib.import_module(flow_fqdn), flow_name)
-        tendrl_ns.add_flow(flow_name, flow_cls)
-
         return maps.NamedDict(atoms=raw_flow['atoms'],
-                            help=raw_flow['help'],
-                            enabled=raw_flow['enabled'],
-                            inputs=raw_flow['inputs'],
-                            pre_run=raw_flow.get('pre_run', []),
-                            post_run=raw_flow.get('post_run', []),
-                            type=raw_flow['type'],
-                            uuid=raw_flow['uuid']
-                            )
+                              help=raw_flow['help'],
+                              enabled=raw_flow['enabled'],
+                              inputs=raw_flow['inputs'],
+                              pre_run=raw_flow.get('pre_run', []),
+                              post_run=raw_flow.get('post_run', []),
+                              type=raw_flow['type'],
+                              uuid=raw_flow['uuid'])
 
     def _get_parsed_defs(self):
         self._parsed_defs = yaml.safe_load(self.master)
