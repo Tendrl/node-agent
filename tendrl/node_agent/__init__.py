f6b442ad141105dfaa54e191d91c812215104633
anmolsachan <anmol13694@gmail.com>
2017-03-02 16:37:33 +0530

    Made node_agent the common logger

--- tendrl/node_agent/__init__.py
+++ tendrl/node_agent/__init__.py
@@ -6,9 +6,13 @@ else:
     monkey.patch_all()
 
 from tendrl.commons import TendrlNS
-
+from tendrl.node_agent import log
 
 class NodeAgentNS(TendrlNS):
     def __init__(self, ns_name="node_agent",
                  ns_src="tendrl.node_agent"):
         super(NodeAgentNS, self).__init__(ns_name, ns_src)
+
+        log.setup_logging(
+            self.config.data['log_cfg_path'],
+        )
