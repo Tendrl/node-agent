f3bd8a76d3679861185bbc023d390eec3b02823a
Rohan Kanade <rkanade@redhat.com>
2017-03-19 02:38:18 +0530

    Node agent sync should also update detected cluster details
    tendrl-bug-id: Tendrl/node-agent#305

--- tendrl/node_agent/manager/__init__.py
+++ tendrl/node_agent/manager/__init__.py
@@ -1,6 +1,5 @@
 import logging
 
-import etcd
 import gevent
 import signal
 
@@ -10,8 +9,6 @@ from tendrl.integrations import ceph
 from tendrl.integrations import gluster
 from tendrl import node_agent
 from tendrl.node_agent import central_store
-from tendrl.node_agent.discovery.platform.manager import PlatformManager
-from tendrl.node_agent.discovery.sds.manager import SDSDiscoveryManager
 from tendrl.node_agent.message.handler import MessageHandler
 from tendrl.node_agent import node_sync
 from tendrl.node_agent.provisioner.ceph.manager import ProvisioningManager
@@ -31,60 +28,8 @@ class NodeAgentManager(commons_manager.Manager):
             NS.message_handler_thread
         )
 
-        self.load_and_execute_platform_discovery_plugins()
-        self.load_and_execute_sds_discovery_plugins()
-
-    def load_and_execute_platform_discovery_plugins(self):
-        # platform plugins
-        LOG.info("load_and_execute_platform_discovery_plugins, platform \
-         plugins")
-        try:
-            pMgr = PlatformManager()
-        except ValueError as ex:
-            LOG.error(
-                'Failed to init PlatformManager. \Error %s', str(ex))
-            return
-        # execute the platform plugins
-        for plugin in pMgr.get_available_plugins():
-            platform_details = plugin.discover_platform()
-            if len(platform_details.keys()) > 0:
-                # update etcd
-                try:
-                    NS.platform = NS.tendrl.objects.Platform(
-                        os=platform_details["Name"],
-                        os_version=platform_details["OSVersion"],
-                        kernel_version=platform_details["KernelVersion"],
-                        )
-                    NS.platform.save()
-
-                except etcd.EtcdException as ex:
-                    LOG.error(
-                        'Failed to update etcd . \Error %s', str(ex))
-                break
-
-    def load_and_execute_sds_discovery_plugins(self):
-        LOG.info("load_and_execute_sds_discovery_plugins")
-        try:
-            sds_discovery_manager = SDSDiscoveryManager()
-        except ValueError as ex:
-            LOG.error(
-                'Failed to init SDSDiscoveryManager. \Error %s', str(ex))
-            return
-
-        # Execute the SDS discovery plugins and tag the nodes with data
-        for plugin in sds_discovery_manager.get_available_plugins():
-            sds_details = plugin.discover_storage_system()
-            if sds_details:
-                try:
-                    NS.tendrl.objects.DetectedCluster(
-                        detected_cluster_id=sds_details.get(
-                            'detected_cluster_id'),
-                        sds_pkg_name=sds_details.get('pkg_name'),
-                        sds_pkg_version=sds_details.get('pkg_version'),
-                    ).save()
-                except etcd.EtcdException as ex:
-                    LOG.error('Failed to update etcd . Error %s', str(ex))
-                break
+        node_sync.platform_detect.load_and_execute_platform_discovery_plugins()
+        node_sync.sds_detect.load_and_execute_sds_discovery_plugins()
 
 
 def main():
