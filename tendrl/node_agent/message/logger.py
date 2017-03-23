e00918ac8ff26755ebb589d9e86003926e7df2d7
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-03-23 12:23:31 +0530

    Update logger.py

--- tendrl/node_agent/message/logger.py
+++ tendrl/node_agent/message/logger.py
@@ -34,7 +34,7 @@ class Logger(object):
     def push_operation(self):
         NS.etcd_orm.client.write(
             "/Messages/jobs/%s" % self.message.job_id,
-            '',
+            None,
             dir=True,
             ttl=NS.config.data['message_retention_time'])
         NS.etcd_orm.client.write(
