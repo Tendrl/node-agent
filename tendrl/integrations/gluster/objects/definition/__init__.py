52e0d382dcb6a0d0900889b18e19e5778a63cb6d
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-03-29 12:41:43 +0530

    Update __init__.py

--- tendrl/integrations/gluster/objects/definition/__init__.py
+++ tendrl/integrations/gluster/objects/definition/__init__.py
@@ -9,10 +9,12 @@ from tendrl.commons import objects
 
 
 class Definition(objects.BaseObject):
+    internal = True
     def __init__(self, *args, **kwargs):
+        self._defs = {}
         super(Definition, self).__init__(*args, **kwargs)
 
-        self.value = '_tendrl/integrations/gluster/definitions'
+        self.value = '_NS/integrations/gluster/definitions'
         self.data = pkg_resources.resource_string(__name__, "gluster.yaml")
         self._parsed_defs = yaml.safe_load(self.data)
         self._etcd_cls = _DefinitionEtcd
@@ -21,13 +23,10 @@ class Definition(objects.BaseObject):
         self._parsed_defs = yaml.safe_load(self.data)
         return self._parsed_defs
 
-    def load_definition(self):
-        return {}
-
 
 class _DefinitionEtcd(etcdobj.EtcdObj):
     """A table of the Definitions, lazily updated
 
     """
-    __name__ = '_tendrl/integrations/gluster/definitions'
+    __name__ = '_NS/integrations/gluster/definitions'
     _tendrl_cls = Definition
