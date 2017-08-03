aa6d63bf3ea132fb5f4f99da5bb7836d3bf15323
anmolbabu <anmolbudugutta@gmail.com>
2017-08-04 05:08:00 +0530

    Add up counters  + Fix delays in staats reporting
    tendrl-bug-id: Tendrl/node-agent#566, Tendrl/node-agent#567
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- tendrl/node_agent/monitoring/collectd/collectors/gluster/low_weight/tendrl_glusterfs_health_counters.py
+++ tendrl/node_agent/monitoring/collectd/collectors/gluster/low_weight/tendrl_glusterfs_health_counters.py
@@ -1,146 +1,199 @@
 import collectd
 import traceback
 
-import tendrl_gluster
 import utils as tendrl_glusterfs_utils
 
 
 PEER_IN_CLUSTER = ['Peer', 'in', 'Cluster']
+CLUSTER_TOPOLOGY = {}
+CONFIG = {}
 
 
-class TendrlGlusterfsHealthCounters(
-    tendrl_gluster.TendrlGlusterfsMonitoringBase
-):
-    def __init__(self):
-        tendrl_gluster.TendrlGlusterfsMonitoringBase.__init__(self)
+def get_peer_count():
+    global CLUSTER_TOPOLOGY
+    global PEER_IN_CLUSTER
+    peer_down_cnt = 0
+    peers = CLUSTER_TOPOLOGY.get('peers', [])
+    # Peer list in get-state output doesn't provide info of current node
+    peer_total_cnt = len(peers) + 1
+    # To test: What if current node glusterd is down ?? or peer is out of
+    # cluster for any reason..
+    for peer in peers:
+        if (
+            not (peer['connected'] == 'Connected') or
+            not (
+                len(
+                    set(peer['state']) & set(PEER_IN_CLUSTER)
+                ) == len(PEER_IN_CLUSTER)
+            )
+        ):
+            peer_down_cnt = peer_down_cnt + 1
+    return (
+        peer_total_cnt,
+        peer_down_cnt,
+        peer_total_cnt - peer_down_cnt
+    )
 
-    def get_peer_count(self):
-        peer_down_cnt = 0
-        peers = self.CLUSTER_TOPOLOGY.get('peers', [])
-        # Peer list in get-state output doesn't provide info of current node
-        peer_total_cnt = len(peers) + 1
-        # To test: What if current node glusterd is down ?? or peer is out of
-        # cluster for any reason..
-        for peer in peers:
-            if (
-                not (peer['connected'] == 'Connected') or
-                not (
-                    len(
-                        set(peer['state']) & set(PEER_IN_CLUSTER)
-                    ) == len(PEER_IN_CLUSTER)
+
+def get_volume_statuses():
+    global CLUSTER_TOPOLOGY
+    ret_val = {}
+    volumes = CLUSTER_TOPOLOGY.get('volumes', [])
+    for volume in volumes:
+        status = volume.get('status', '')
+        ret_val[volume['name']] = \
+            tendrl_glusterfs_utils.get_volume_state_mapping(status)
+    return ret_val
+
+
+def get_volume_status_wise_counts():
+    global CLUSTER_TOPOLOGY
+    volumes = CLUSTER_TOPOLOGY.get('volumes', [])
+    volume_total_cnt = len(volumes)
+    volume_up_count = 0
+    for volume in volumes:
+        if volume.get('status') == 'Started':
+            volume_up_count = volume_up_count + 1
+    return (
+        volume_total_cnt,
+        volume_total_cnt - volume_up_count,
+        volume_up_count
+    )
+
+
+def get_volume_brick_statuses(volume):
+    ret_val = {}
+    for sub_volume_index, sub_volume_bricks in volume.get(
+        'bricks',
+        {}
+    ).iteritems():
+        for brick in sub_volume_bricks:
+            if 'status' in brick:
+                brick_statuses = ret_val.get(brick['hostname'], [])
+                b_path = tendrl_glusterfs_utils.get_brick_state_mapping(
+                    brick['status']
                 )
-            ):
-                peer_down_cnt = peer_down_cnt + 1
-        return peer_total_cnt, peer_down_cnt
+                brick_statuses.append(
+                    {
+                        brick['path']: b_path
+                    }
+                )
+                ret_val['hostname'] = brick_statuses
+    return ret_val
 
-    def get_volume_statuses(self):
+
+def get_metrics():
+    global CLUSTER_TOPOLOGY
+    global CONFIG
+    try:
         ret_val = {}
-        volumes = self.CLUSTER_TOPOLOGY.get('volumes', [])
+        # Push Peer status wise and total counts
+        ret_val[
+            'clusters.%s.nodes_count.total' % (
+                CONFIG['integration_id']
+            )
+        ], ret_val[
+            'clusters.%s.nodes_count.down' % (
+                CONFIG['integration_id']
+            )
+        ], ret_val[
+            'clusters.%s.nodes_count.up' % (
+                CONFIG['integration_id']
+            )
+        ] = get_peer_count()
+        # Push brick statuses
+        volumes = CLUSTER_TOPOLOGY.get('volumes', [])
         for volume in volumes:
-            status = volume.get('status', '')
-            ret_val[volume['name']] = \
-                tendrl_glusterfs_utils.get_volume_state_mapping(status)
-        return ret_val
-
-    def get_volume_status_wise_counts(self):
-        volumes = self.CLUSTER_TOPOLOGY.get('volumes', [])
-        volume_total_cnt = len(volumes)
-        volume_down_count = 0
+            brick_statuses = get_volume_brick_statuses(volume)
+            for host_name, brick in brick_statuses.iteritems():
+                for path, status_val in brick.iteritems():
+                    ret_val[
+                        'clusters.%s.volumes.%s.bricks_count' % (
+                            CONFIG['integration_id'],
+                            volume.get('name', ''),
+                        )
+                    ] = status_val
+        # Push volume statuses
+        volume_statuses = get_volume_statuses()
+        for vol_name, status_val in volume_statuses.iteritems():
+            ret_val[
+                'clusters.%s.volumes.%s.status' % (
+                    CONFIG['integration_id'],
+                    vol_name
+                )
+            ] = status_val
+        # Push volume status wise counts
+        ret_val[
+            'clusters.%s.volume_count.total' % (
+                CONFIG['integration_id']
+            )
+        ], ret_val[
+            'clusters.%s.volume_count.down' % (
+                CONFIG['integration_id']
+            )
+        ], ret_val[
+            'clusters.%s.volume_count.up' % (
+                CONFIG['integration_id']
+            )
+        ] = get_volume_status_wise_counts()
+        # Push Volume wise brick total count
         for volume in volumes:
-            if volume.get('status') is not 'Started':
-                volume_down_count = volume_down_count + 1
-        return volume_total_cnt, volume_down_count
-
-    def get_volume_brick_statuses(self, volume):
-        ret_val = {}
-        for sub_volume_index, sub_volume_bricks in volume.get(
-            'bricks',
-            {}
-        ).iteritems():
-            for brick in sub_volume_bricks:
-                if 'status' in brick:
-                    brick_statuses = ret_val.get(brick['hostname'], [])
-                    b_path = tendrl_glusterfs_utils.get_brick_state_mapping(
-                        brick['status']
-                    )
-                    brick_statuses.append(
-                        {
-                            brick['path']: b_path
-                        }
-                    )
-                    ret_val['hostname'] = brick_statuses
-        return ret_val
-
-    def get_metrics(self):
-        try:
-            ret_val = {}
-            # Push Peer status wise and total counts
             ret_val[
-                'clusters.%s.nodes_count.total' % (
-                    self.CONFIG['integration_id']
+                'clusters.%s.volumes.%s.brick_count.total' % (
+                    CONFIG['integration_id'],
+                    vol_name
                 )
-            ], ret_val[
-                'clusters.%s.nodes_count.down' % (
-                    self.CONFIG['integration_id']
-                )
-            ] = self.get_peer_count()
-            # Push brick statuses
-            volumes = self.CLUSTER_TOPOLOGY.get('volumes', [])
-            for volume in volumes:
-                brick_statuses = self.get_volume_brick_statuses(volume)
-                for host_name, brick in brick_statuses.iteritems():
-                    for path, status_val in brick.iteritems():
-                        ret_val[
-                            'clusters.%s.volumes.%s.bricks_count' % (
-                                self.CONFIG['integration_id'],
-                                volume.get('name', ''),
-                            )
-                        ] = status_val
-            # Push volume statuses
-            volume_statuses = self.get_volume_statuses()
-            for vol_name, status_val in volume_statuses.iteritems():
-                ret_val[
-                    'clusters.%s.volumes.%s.status' % (
-                        self.CONFIG['integration_id'],
-                        vol_name
-                    )
-                ] = status_val
-            # Push volume status wise counts
-            ret_val[
-                'clusters.%s.volume_count.total' % (
-                    self.CONFIG['integration_id']
-                )
-            ], ret_val[
-                'clusters.%s.volume_count.down' % (
-                    self.CONFIG['integration_id']
-                )
-            ] = self.get_volume_status_wise_counts()
-            # Push Volume wise brick total count
-            for volume in volumes:
-                ret_val[
-                    'clusters.%s.volumes.%s.brick_count.total' % (
-                        self.CONFIG['integration_id'],
-                        vol_name
-                    )
-                ] = int(volume.get('brick_count', 0))
-            # Push cluster wise brick total count
-            cluster_brick_count = 0
-            for volume in volumes:
-                cluster_brick_count = cluster_brick_count + int(
-                    volume.get(
-                        'brick_count',
-                        0
-                    )
-                )
-            ret_val[
-                'clusters.%s.brick_count.total' % (
-                    self.CONFIG['integration_id']
-                )
-            ] = cluster_brick_count
-            return ret_val
-        except (AttributeError, KeyError, ValueError):
-            collectd.error(
-                'Failed to fetch counters. Error %s\n\n' % (
-                    traceback.format_exc()
+            ] = int(volume.get('brick_count', 0))
+        # Push cluster wise brick total count
+        cluster_brick_count = 0
+        for volume in volumes:
+            cluster_brick_count = cluster_brick_count + int(
+                volume.get(
+                    'brick_count',
+                    0
                 )
             )
+        ret_val[
+            'clusters.%s.brick_count.total' % (
+                CONFIG['integration_id']
+            )
+        ] = cluster_brick_count
+        return ret_val
+    except (AttributeError, KeyError, ValueError):
+        collectd.error(
+            'Failed to fetch counters. Error %s\n\n' % (
+                traceback.format_exc()
+            )
+        )
+
+
+def read_callback():
+    global CLUSTER_TOPOLOGY
+    global CONFIG
+    CLUSTER_TOPOLOGY = \
+        tendrl_glusterfs_utils.get_gluster_cluster_topology()
+    metrics = get_metrics()
+    for metric_name, value in metrics.iteritems():
+        if value is not None:
+            if (
+                isinstance(value, str) and
+                value.isdigit()
+            ):
+                value = int(value)
+            tendrl_glusterfs_utils.write_graphite(
+                metric_name,
+                value,
+                CONFIG['graphite_host'],
+                CONFIG['graphite_port']
+            )
+
+
+def configure_callback(configobj):
+    global CONFIG
+    CONFIG = {
+        c.key: c.values[0] for c in configobj.children
+    }
+
+
+collectd.register_config(configure_callback)
+collectd.register_read(read_callback, 60)
--- tendrl/node_agent/monitoring/collectd/collectors/gluster/low_weight/tendrl_glusterfs_profile_info.py
+++ tendrl/node_agent/monitoring/collectd/collectors/gluster/low_weight/tendrl_glusterfs_profile_info.py
@@ -1,167 +1,209 @@
 import collectd
+import gevent
 import traceback
 try:
     import xml.etree.cElementTree as ElementTree
 except ImportError:
     import xml.etree.ElementTree as ElementTree
 
-import tendrl_gluster
 import utils as tendrl_glusterfs_utils
 
 
-class TendrlGlusterfsProfileInfo(
-    tendrl_gluster.TendrlGlusterfsMonitoringBase
-):
-    def __init__(self):
-        tendrl_gluster.TendrlGlusterfsMonitoringBase.__init__(self)
+PLUGIN_NAME = 'network_throughput'
+CONFIG = {}
+CLUSTER_TOPOLOGY = {}
 
-    def _parseVolumeProfileInfo(self, tree, nfs=False):
-        bricks = []
-        if nfs:
-            brickKey = 'nfs'
-            bricksKey = 'nfsServers'
-        else:
-            brickKey = 'brick'
-            bricksKey = 'bricks'
-        for brick in tree.findall('volProfile/brick'):
-            fopCumulative = []
-            blkCumulative = []
-            fopInterval = []
-            blkInterval = []
-            brickName = brick.find('brickName').text
-            for block in brick.findall('cumulativeStats/blockStats/block'):
-                blkCumulative.append(
-                    {
-                        'size': block.find('size').text,
-                        'read': block.find('reads').text,
-                        'write': block.find('writes').text
-                    }
-                )
-            for fop in brick.findall('cumulativeStats/fopStats/fop'):
-                fopCumulative.append(
-                    {
-                        'name': fop.find('name').text,
-                        'hits': fop.find('hits').text,
-                        'latencyAvg': fop.find('avgLatency').text,
-                        'latencyMin': fop.find('minLatency').text,
-                        'latencyMax': fop.find('maxLatency').text
-                    }
-                )
-            for block in brick.findall('intervalStats/blockStats/block'):
-                blkInterval.append(
-                    {
-                        'size': block.find('size').text,
-                        'read': block.find('reads').text,
-                        'write': block.find('writes').text
-                    }
-                )
-            for fop in brick.findall('intervalStats/fopStats/fop'):
-                fopInterval.append(
-                    {
-                        'name': fop.find('name').text,
-                        'hits': fop.find('hits').text,
-                        'latencyAvg': fop.find('avgLatency').text,
-                        'latencyMin': fop.find('minLatency').text,
-                        'latencyMax': fop.find('maxLatency').text
-                    }
-                )
-            bricks.append(
+
+def _parseVolumeProfileInfo(tree, nfs=False):
+    bricks = []
+    if nfs:
+        brickKey = 'nfs'
+        bricksKey = 'nfsServers'
+    else:
+        brickKey = 'brick'
+        bricksKey = 'bricks'
+    for brick in tree.findall('volProfile/brick'):
+        fopCumulative = []
+        blkCumulative = []
+        fopInterval = []
+        blkInterval = []
+        brickName = brick.find('brickName').text
+        for block in brick.findall('cumulativeStats/blockStats/block'):
+            blkCumulative.append(
                 {
-                    brickKey: brickName,
-                    'cumulativeStats': {
-                        'blockStats': blkCumulative,
-                        'fopStats': fopCumulative,
-                        'duration': brick.find(
-                            'cumulativeStats/duration'
-                        ).text,
-                        'totalRead': brick.find(
-                            'cumulativeStats/totalRead'
-                        ).text,
-                        'totalWrite': brick.find(
-                            'cumulativeStats/totalWrite'
-                        ).text
-                    },
-                    'intervalStats': {
-                        'blockStats': blkInterval,
-                        'fopStats': fopInterval,
-                        'duration': brick.find('intervalStats/duration').text,
-                        'totalRead': brick.find(
-                            'intervalStats/totalRead'
-                        ).text,
-                        'totalWrite': brick.find(
-                            'intervalStats/totalWrite'
-                        ).text
-                    }
+                    'size': block.find('size').text,
+                    'read': block.find('reads').text,
+                    'write': block.find('writes').text
                 }
             )
-        status = {
-            'volumeName': tree.find("volProfile/volname").text,
-            bricksKey: bricks
-        }
-        return status
-
-    def get_volume_profile_info(self, volName):
-        ret_val = {}
-        brickName = ''
-        profile_info = {}
-        profile_cmd_op, profile_err = tendrl_glusterfs_utils.exec_command(
-            "gluster volume profile %s info --xml" % volName
+        for fop in brick.findall('cumulativeStats/fopStats/fop'):
+            gevent.sleep(0.1)
+            fopCumulative.append(
+                {
+                    'name': fop.find('name').text,
+                    'hits': fop.find('hits').text,
+                    'latencyAvg': fop.find('avgLatency').text,
+                    'latencyMin': fop.find('minLatency').text,
+                    'latencyMax': fop.find('maxLatency').text
+                }
+            )
+        for block in brick.findall('intervalStats/blockStats/block'):
+            gevent.sleep(0.1)
+            blkInterval.append(
+                {
+                    'size': block.find('size').text,
+                    'read': block.find('reads').text,
+                    'write': block.find('writes').text
+                }
+            )
+        for fop in brick.findall('intervalStats/fopStats/fop'):
+            gevent.sleep(0.1)
+            fopInterval.append(
+                {
+                    'name': fop.find('name').text,
+                    'hits': fop.find('hits').text,
+                    'latencyAvg': fop.find('avgLatency').text,
+                    'latencyMin': fop.find('minLatency').text,
+                    'latencyMax': fop.find('maxLatency').text
+                }
+            )
+        bricks.append(
+            {
+                brickKey: brickName,
+                'cumulativeStats': {
+                    'blockStats': blkCumulative,
+                    'fopStats': fopCumulative,
+                    'duration': brick.find(
+                        'cumulativeStats/duration'
+                    ).text,
+                    'totalRead': brick.find(
+                        'cumulativeStats/totalRead'
+                    ).text,
+                    'totalWrite': brick.find(
+                        'cumulativeStats/totalWrite'
+                    ).text
+                },
+                'intervalStats': {
+                    'blockStats': blkInterval,
+                    'fopStats': fopInterval,
+                    'duration': brick.find('intervalStats/duration').text,
+                    'totalRead': brick.find(
+                        'intervalStats/totalRead'
+                    ).text,
+                    'totalWrite': brick.find(
+                        'intervalStats/totalWrite'
+                    ).text
+                }
+            }
         )
-        if profile_err:
-            collectd.error(
-                'Failed to fetch brick utilizations. The error is: %s' % (
-                    profile_err
-                )
-            )
-            return ret_val
-        try:
-            profile_info = self._parseVolumeProfileInfo(
-                ElementTree.fromstring(profile_cmd_op)
-            )
-            return profile_info
-        except (
-            AttributeError,
-            KeyError,
-            ValueError
-        ):
-            collectd.error(
-                'Failed to collect iops details of brick %s in volume %s of '
-                'cluster %s. The profile info is %s. Error %s' % (
-                    brickName,
-                    volName,
-                    self.CONFIG['integration_id'],
-                    str(profile_info),
-                    traceback.format_exc()
-                )
-            )
-            return ret_val
+    status = {
+        'volumeName': tree.find("volProfile/volname").text,
+        bricksKey: bricks
+    }
+    return status
 
-    def get_metrics(self):
-        ret_val = {}
-        volumes = self.CLUSTER_TOPOLOGY.get('volumes', [])
-        for volume in volumes:
-            volName = volume['name']
-            vol_iops = self.get_volume_profile_info(volName)
-            for brick_det in vol_iops.get('bricks'):
-                brickName = brick_det.get('brick')
-                t_name = "clusters.%s.volumes.%s.nodes.%s.bricks.%s.iops." \
-                    "gauge-read"
-                ret_val[
-                    t_name % (
-                        self.CONFIG['integration_id'],
-                        volName,
-                        brickName.split(':')[0].replace('.', '_'),
-                        brickName.split(':')[1].replace('/', '|')
-                    )
-                ] = brick_det.get('cumulativeStats').get('totalRead')
-                t_name = "clusters.%s.volumes.%s.nodes.%s.bricks.%s.iops." \
-                    "gauge-write"
-                ret_val[
-                    t_name % (
-                        self.CONFIG['integration_id'],
-                        volName,
-                        brickName.split(':')[0].replace('.', '_'),
-                        brickName.split(':')[1].replace('/', '|')
-                    )
-                ] = brick_det.get('cumulativeStats').get('totalWrite')
+
+def get_volume_profile_info(volName):
+    global CONFIG
+    ret_val = {}
+    brickName = ''
+    profile_info = {}
+    profile_cmd_op, profile_err = tendrl_glusterfs_utils.exec_command(
+        "gluster volume profile %s info --xml" % volName
+    )
+    if profile_err:
+        collectd.error(
+            'Failed to fetch brick utilizations. The error is: %s' % (
+                profile_err
+            )
+        )
         return ret_val
+    try:
+        profile_info = _parseVolumeProfileInfo(
+            ElementTree.fromstring(profile_cmd_op)
+        )
+        return profile_info
+    except (
+        AttributeError,
+        KeyError,
+        ValueError
+    ):
+        collectd.error(
+            'Failed to collect iops details of brick %s in volume %s of '
+            'cluster %s. The profile info is %s. Error %s' % (
+                brickName,
+                volName,
+                CONFIG['integration_id'],
+                str(profile_info),
+                traceback.format_exc()
+            )
+        )
+        return ret_val
+
+
+def get_metrics():
+    global CONFIG
+    global CLUSTER_TOPOLOGY
+    ret_val = {}
+    volumes = CLUSTER_TOPOLOGY.get('volumes', [])
+    for volume in volumes:
+        volName = volume['name']
+        vol_iops = get_volume_profile_info(volName)
+        if not vol_iops:
+            return ret_val
+        for brick_det in vol_iops.get('bricks', {}):
+            brickName = brick_det.get('brick', '')
+            t_name = "clusters.%s.volumes.%s.nodes.%s.bricks.%s.iops." \
+                "gauge-read"
+            ret_val[
+                t_name % (
+                    CONFIG['integration_id'],
+                    volName,
+                    brickName.split(':')[0].replace('.', '_'),
+                    brickName.split(':')[1].replace('/', '|')
+                )
+            ] = brick_det.get('cumulativeStats').get('totalRead')
+            t_name = "clusters.%s.volumes.%s.nodes.%s.bricks.%s.iops." \
+                "gauge-write"
+            ret_val[
+                t_name % (
+                    CONFIG['integration_id'],
+                    volName,
+                    brickName.split(':')[0].replace('.', '_'),
+                    brickName.split(':')[1].replace('/', '|')
+                )
+            ] = brick_det.get('cumulativeStats').get('totalWrite')
+            gevent.sleep(0.1)
+    return ret_val
+
+
+def read_callback():
+    global CLUSTER_TOPOLOGY
+    global CONFIG
+    CLUSTER_TOPOLOGY = \
+        tendrl_glusterfs_utils.get_gluster_cluster_topology()
+    metrics = get_metrics()
+    for metric_name, value in metrics.iteritems():
+        if value is not None:
+            if (
+                isinstance(value, str) and
+                value.isdigit()
+            ):
+                value = int(value)
+            tendrl_glusterfs_utils.write_graphite(
+                metric_name,
+                value,
+                CONFIG['graphite_host'],
+                CONFIG['graphite_port']
+            )
+
+
+def configure_callback(configobj):
+    global CONFIG
+    CONFIG = {
+        c.key: c.values[0] for c in configobj.children
+    }
+
+
+collectd.register_config(configure_callback)
+collectd.register_read(read_callback, 60)
