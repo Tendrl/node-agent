289ac64d78186f90512eaed15d8359e150faed4b
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-08-04 03:17:40 +0530

    Update setup.py
    tendrl-bug-id: Tendrl/node-agent/issues#561

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ setup(
     entry_points={
         'console_scripts': [
             'tendrl-node-agent = tendrl.node_agent.manager:main',
-            'tendrl_monitoring_config_manager = tendrl.node_agent.monitoring.collectd.commands.config_manager:main'
+            'tendrl-monitoring-config-manager = tendrl.node_agent.monitoring.collectd.commands.config_manager:main'
         ]
     },
     include_package_data=True,
