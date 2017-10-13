31f55de7d0c96a52c9c2e5654f4c9bbc2ed681ae
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-10-13 10:56:35 +0530

    Update check_all_managed_nodes_status.py

--- tendrl/node_agent/node_sync/check_all_managed_nodes_status.py
+++ tendrl/node_agent/node_sync/check_all_managed_nodes_status.py
@@ -17,16 +17,4 @@ def run():
             )            
         except etcd.EtcdAlreadyExist:
             pass
-        try:
-            _tc = NS.tendrl.objects.TendrlContext(node_id=node_id).load()
-            if _tc.integration_id:
-                NS._int.client.write(
-                    "/clusters/{0}/nodes/{1}/NodeContext/status".format(_tc.integration_id,
-                                                                    node_id),
-                    "DOWN",
-                    prevExist=False
-                )            
-        except etcd.EtcdAlreadyExist:
-            pass
-
     return
