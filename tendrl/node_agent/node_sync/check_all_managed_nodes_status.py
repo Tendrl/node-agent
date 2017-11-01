636241c560d86f27ecd1e8f7f9a193f6ecbc0a78
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-11-01 16:11:24 +0530

    Update check_all_managed_nodes_status.py
    tendrl-bug-id: Tendrl/gluster-integration#436

--- tendrl/node_agent/node_sync/check_all_managed_nodes_status.py
+++ tendrl/node_agent/node_sync/check_all_managed_nodes_status.py
@@ -1,5 +1,7 @@
 import etcd
 
+from tendrl.commons.utils import etcd_utils
+
 
 def run():
     try:
@@ -15,6 +17,19 @@ def run():
                 "DOWN",
                 prevExist=False
             )
+            _node_context = NS.tendrl.objects.NodeContext(node_id=node_id).load()
+            _tc = NS.tendrl.objects.TendrlContext(node_id=node_id).load()
+            _cluster = NS.tendrl.objects.Cluster(integration_id=_tc.integration_id).load()
+            
+            # Remove stale provisioner tag
+            if _cluster.is_managed == "yes":
+                _tag = "provisioner/%s" % _cluster.integration_id
+                if _tag in _node_context.tags:
+                    _index_key = "/indexes/tags/%s" % _tag
+                    _node_context.tags.remove(_tag)
+                    _node_context.save()
+                    etcd_utils.delete(_index_key)
+                    
         except etcd.EtcdAlreadyExist:
             pass
     return
